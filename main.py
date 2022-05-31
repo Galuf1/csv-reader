e0ee135b33a9a9e96df1597c98204b27271df654
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-31 23:56:06 +0200

    solved with OOP

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import csv
 from socket import create_server
 
+class animal:
+    def __init__(self,name,age=None,breed=None):
+        self.name = name
+        self.age = age
+        self.breed = breed
+
+    def __str__(self):
+            return (f"{self.name} is a{self.age} year old{self.breed}.")
+
 animal_getter = input("type of animal ")
-animal = animal_getter
-file = f"data/{animal}.csv"
+animal_type = animal_getter
+file = f"data/{animal_type}.csv"
 data = {} 
 try:
     with open(file) as csvfile:
         csvreader =  csv.reader(csvfile,delimiter=",")
+        # populate dictionary data with name as key and other as a list of attributes
         for row in csvreader:
             key = row[0]
-            value = f"{row[0]} is a{row[1]} year old{row[2]}."
-            data[key]=value
-    data.pop("name")
+            age_data = row[1]
+            breed_data = row[2]
+            data[key]= [age_data,breed_data]
+    # removes first row
+    poppedKeys = data.pop("name")
     pet_name = str(input("name of the pet "))
-
-
-    print(data[pet_name])
+    query = animal(pet_name,data[pet_name][0],data[pet_name][1])
+    print(query)
+    
 except:
-    print(f"Sorry, we don't have any {animal} here")
+    print(f"Sorry, we don't have any {animal_type} here")
 
 
 
@@ -26,18 +38,10 @@ except:
 
         
 
-class animal:
-    def __init__(self,type,name,age):
-        self.name = name
-        self.type = type
-        self.age = age
-
-    def __str__(self):
-            return (f"{self.name} is a {self.age} year old {self.type}.")
 
 
-# fido = animal("fido", 4, "husky")
-# print(fido) 
+
+
 
 
 
